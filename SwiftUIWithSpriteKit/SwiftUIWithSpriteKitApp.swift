92bf27f93827a9bbca7267df8632ed9b2fca066a
DoodMac <48062579+jbird84@users.noreply.github.com>
2022-11-17 19:51:17 -0600

    Initial Commit

---
+++ SwiftUIWithSpriteKit/SwiftUIWithSpriteKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithSpriteKitApp.swift
+//  SwiftUIWithSpriteKit
+//
+//  Created by Kinney Kare on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithSpriteKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
